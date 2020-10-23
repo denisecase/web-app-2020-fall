d4eb9877b2ea86aa28078533f800fdecd3899744
KarepeN <s540109@nwmissouri.edu>
2020-10-22 23:37:00 -0500

    series initial - Nithya Karepe

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -1,15 +1,15 @@
 /**
- *  Tea controller
- *  Handles requests related to tea (see routes)
+ *  series controller
+ *  Handles requests related to series(see routes)
  *
- * @author Charles Hoot <hoot@nwmissouri.edu>
+ * @author Nithya Karepe <s540109@nwmissouri.edu>
  */
 
 // import dependencies
 const express = require('express');
 
 // import local code files
-// const model = require('../models/tea.js');
+// const model = require('../models/series.js');
 
 // create a router
 const router = express.Router();
