1942c3927e0535c39debfb964b2e231286c0f259
Jack Beaver <jack.beaver25@gmail.com>
2020-10-22 21:03:33 -0500

    Added chief files

---
+++ controllers/chiefController.js
@@ -0,0 +1,17 @@
+/**
+ *  Chiefs controller
+ *  Handles requests related to chief (see routes)
+ *
+ * @author Jack W Beaver <s526937@nwmissouri.edu>
+ */
+
+// import dependencies
+const express = require('express');
+
+// import local code files
+// const model = require('../models/chief.js');
+
+// create a router
+const router = express.Router();
+
+module.exports = router;
