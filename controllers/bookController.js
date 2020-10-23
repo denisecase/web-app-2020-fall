24f5152dc7e557f7e263dd2b10112e815d74ee74
shivani <tangellapallyshivani@gmail.com>
2020-10-23 17:03:18 -0500

    initial commit

---
+++ controllers/bookController.js
@@ -0,0 +1,17 @@
+/**
+ *  book controller
+ *  Handles requests related to book (see routes)
+ *
+ * @author shivani tangellapally <s540965@nwmissouri.edu>
+ */
+
+// import dependencies
+const express = require('express');
+
+// import local code files
+// const model = require('../models/book.js');
+
+// create a router
+const router = express.Router();
+
+module.exports = router;
