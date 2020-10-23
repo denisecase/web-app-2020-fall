ed42ded7a4b4f757b0fa46c5aa3230c641b45ef6
Kunal <70285661+kvohra93@users.noreply.github.com>
2020-10-23 14:05:08 -0500

    videogame controller and model added

---
+++ controllers/videoGameController.js
@@ -0,0 +1,17 @@
+/**
+ *  videogame controller
+ *  Handles requests related to videogame (see routes)
+ *
+ * @author kunal vohra <s540786@nwmissouri.edu>
+ */
+
+// import dependencies
+const express = require('express');
+
+// import local code files
+// const model = require('../models/videogame.js');
+
+// create a router
+const router = express.Router();
+
+module.exports = router;
---
+++ models/video-game.js
@@ -0,0 +1,6 @@
+/**
+ *  video-games model
+ *  Describes the characteristics of each attribute in a animal resource.
+ *
+ * @author kunal vohra <s540786@nwmissouri.edu>
+ */
