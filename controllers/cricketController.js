57d56cba1f45d0c70b0a2fd7d98c3754f48480f7
Praneeth <pranethv@gmail.com>
2020-10-23 20:16:56 -0500

    Added model view and controller for cricket team

---
+++ controllers/cricketController.js
@@ -0,0 +1,17 @@
+/**
+ *  cricket controller
+ *  Handles requests related to plant (see routes)
+ *
+ * @author Praneeth Vallabhaneni <S541312@nwmissouri.edu>
+ */
+
+// import dependencies
+const express = require('express');
+
+// import local code files
+// const model = require('../models/cricket.js');
+
+// create a router
+const router = express.Router();
+
+module.exports = router;
