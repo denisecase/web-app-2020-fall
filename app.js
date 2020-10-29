842db20405fea0f27ff733650dc220145340271e
Dobelmann <S536997@nwmissouri.edu>
2020-10-28 20:41:13 -0500

    Modified app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use('/animal', require('./routes/animal.routes'));
 // Sri Vasavi - food
 
 // Joseph - software
-
+ app.use('/software', require('./routes/software.routes'));
 // Stephen - whiskey
 app.use('/whiskey', require('./routes/whiskey.routes'));
 
