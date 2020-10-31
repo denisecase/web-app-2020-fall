c6c1be018d14dfa99cbcdcb104b258dcbf2efed0
Denise Case <denisecase@gmail.com>
2020-10-31 13:36:57 -0500

    fix config.json to .js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@
 const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require('../config/config')[env];
 
 const db = new Sequelize(config);
 
