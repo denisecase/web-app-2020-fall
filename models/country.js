18c75a36a87dfc5958eb1ef3d159c315fa4e2457
FelipeGHB <46767469+FelipeGHB@users.noreply.github.com>
2020-11-09 23:51:23 -0600

    Source and Implemetation Added

--- models/country.js
+++ models/country.js
@@ -4,3 +4,18 @@
  *
  * @author Felipe Sato  <s531355@nwmissouri.edu>
  */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
+
+module.exports = (sequelize, DataTypes) => {
+    sequelize.define('Country', {
+      continent: { type: DataTypes.STRING(20) },
+      population: { type: DataTypes.INTEGER},
+    });
+  };
+
+  /**
+  name = USA, continent = North America, Pupulation 328 (in million)
+  name = Canada  continent = North America, Pupulation 30
+  name = Mexico  continent = North America, Pupulation 128
+   */
