edb50646c11d41627dbc91f5f8c1f761f67baf37
KarepeN <s540109@nwmissouri.edu>
2020-11-17 21:29:30 -0600

    working local

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -5,13 +5,43 @@
  * @author Nithya Karepe <s540109@nwmissouri.edu>
  */
 
-const db = require('../models/index');
+// OPTIONAL: If using Sequelize validation features
+const { ValidationError } = require('sequelize');
+
+const LOG = require('../util/logger');
+
+const db = require('../models/index')();
+
+// OPTIONAL: VALIDATION Helper function ----------------------
+
+/**
+ * Prepare an item from the request information and add
+ * an 'error' attribute to share with the view.
+ *
+ * @param {*} err - the error
+ * @param {*} req - the request
+ * @returns - the item to attach to response.locals
+ */
+async function prepareInvalidItem(err, req) {
+  LOG.error('ERROR SAVING ITEM');
+  LOG.error('Captured validation error: ', err.errors[0].message);
+  const item = {};
+  if (req.body.id) {
+    item.id = req.body.id;
+  }
+  item.name = req.body.name;
+  item.seasons = req.body.seasons;
+  item.isComedy = req.body.isComedy;
+  item.error = err.errors[0].message;
+  LOG.info(`ERROR SAVING ITEM: ${JSON.stringify(item)}`);
+  return item;
+}
 
 // FUNCTIONS TO RESPOND WITH JSON DATA  ----------------------------------------
 
 // GET all JSON
-module.exports.findAll = (req, res) => {
-  db.models.Series.findAll()
+module.exports.findAll = async (req, res) => {
+  (await db).models.Series.findAll()
     .then((data) => {
       res.send(data);
     })
@@ -23,9 +53,9 @@ module.exports.findAll = (req, res) => {
 };
 
 // GET one JSON by ID
-module.exports.findOne = (req, res) => {
+module.exports.findOne = async (req, res) => {
   const { id } = req.params;
-  db.models.Series.findByPk(id)
+  (await db).models.Series.findByPk(id)
     .then((data) => {
       res.send(data);
     })
@@ -41,71 +71,129 @@ module.exports.findOne = (req, res) => {
 // POST /save
 module.exports.saveNew = async (req, res) => {
   try {
-    await db.models.Series.create(req.body);
+    const context = await db;
+    await context.models.Series.create(req.body);
     return res.redirect('/series');
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      const item = await prepareInvalidItem(err, req);
+      res.locals.series = item;
+      return res.render('series/create.ejs', { title: 'Series', res });
+    }
+    return res.redirect('/series');
   }
 };
 
 // POST /save/:id
 module.exports.saveEdit = async (req, res) => {
   try {
-    const { reqId } = req.params.id;
-    const [updated] = await db.models.Series.update(req.body, {
+    const reqId = parseInt(req.params.id, 10);
+    const context = await db;
+    const updated = await context.models.Series.update(req.body, {
       where: { id: reqId },
     });
-    if (updated) {
-      return res.redirect('/series');
+    LOG.info(`Updated: ${JSON.stringify(updated)}`);
+    return res.redirect('/series');
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      const item = await prepareInvalidItem(err, req);
+      res.locals.series = item;
+      return res.render('series/edit.ejs', { title: 'Series', res });
     }
-    throw new Error(`${reqId} not found`);
-  } catch (error) {
-    return res.status(500).send(error.message);
+    return res.redirect('/series');
   }
 };
 
 // POST /delete/:id
 module.exports.deleteItem = async (req, res) => {
   try {
-    const { reqId } = req.params.seriesId;
-    const deleted = await db.models.Series.destroy({
+    const reqId = parseInt(req.params.id, 10);
+    const deleted = (await db).models.Series.destroy({
       where: { id: reqId },
     });
     if (deleted) {
       return res.redirect('/series');
     }
     throw new Error(`${reqId} not found`);
-  } catch (error) {
-    return res.status(500).send(error.message);
+  } catch (err) {
+    return res.status(500).send(err.message);
   }
 };
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
 // GET to this controller base URI (the default)
-module.exports.showIndex = (req, res) => {
-  // res.send('NOT IMPLEMENTED: Will show series/index.ejs');
-  res.render('series/index.ejs', { title: 'Seriess', req });
+module.exports.showIndex = async (req, res) => {
+  (await db).models.Series.findAll()
+    .then((data) => {
+      res.locals.series = data;
+      res.render('series/index.ejs', { title: 'Series', res });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || 'Error retrieving all.',
+      });
+    });
 };
 
 // GET /create
-module.exports.showCreate = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Will show series/create.ejs for ${req.params.id}`);
+module.exports.showCreate = async (req, res) => {
+  // create a temp series and add it to the response.locals object
+  // this will provide a series object to put any validation errors
+  const tempItem = {
+    name: 'SeriesName',
+    seasons: 1,
+    isComedy: true,
+  };
+  res.locals.series = tempItem;
+  res.render('series/create.ejs', { title: 'Series', res });
 };
 
 // GET /delete/:id
-module.exports.showDelete = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Will show series/delete.ejs for ${req.params.id}`);
+module.exports.showDelete = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Series.findByPk(id)
+    .then((data) => {
+      res.locals.series = data;
+      if (data) {
+        res.render('series/delete.ejs', { title: 'Series', res });
+      } else {
+        res.redirect('series/');
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
 };
 
 // GET /details/:id
-module.exports.showDetails = (req, res) => {
-  res.send(
-    `NOT IMPLEMENTED: Will show series/details.ejs for ${req.params.id}`,
-  );
+module.exports.showDetails = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Series.findByPk(id)
+    .then((data) => {
+      res.locals.series = data;
+      res.render('series/details.ejs', { title: 'Series', res });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
 };
 
 // GET /edit/:id
-module.exports.showEdit = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Will show series/edit.ejs for ${req.params.id}`);
+module.exports.showEdit = async (req, res) => {
+  const { id } = req.params;
+  (await db).models.Series.findByPk(id)
+    .then((data) => {
+      res.locals.series = data;
+      res.render('series/edit.ejs', { title: 'Series', res });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: `Error retrieving item with id=${id}: ${err.message}`,
+      });
+    });
 };
