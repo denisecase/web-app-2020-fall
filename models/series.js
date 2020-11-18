31b20fef85815bd4b2485cc67bb8f52512e96bc4
KarepeN <s540109@nwmissouri.edu>
2020-11-17 22:06:44 -0600

    update

--- models/series.js
+++ models/series.js
@@ -3,7 +3,17 @@
  *  Describes the characteristics of each attribute in a series resource.
  *
  * @author Nithya Karepe<s540109@nwmissouri.edu>
+ *
+ * For more information about defining sequelize models, see
+ * https://sequelize.org/v5/manual/data-types.html
+ * https://sequelize.org/master/manual/validations-and-constraints.html
+ *
+ * For validators see: https://github.com/validatorjs/validator.js
+ *
  */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
+
 module.exports = (db, DataTypes) => {
   db.define('Series', {
     // sqlite creates a rowid attribute automatically
