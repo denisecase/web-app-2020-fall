5c858f23108d24ac633a60430534d302d34b650c
Blake Bennett <s532542@nwmissouri.edu>
2020-11-20 11:26:51 -0600

    changed the name of rabbit to game to match resource - Blake

--- models/game.js
+++ models/game.js
@@ -1,6 +1,6 @@
 /**
- *  Rabbit model
- *  Describes the characteristics of each attribute in a rabbit resource.
+ *  Game model
+ *  Describes the characteristics of each attribute in a game resource.
  *
  * @author Blake Bennett <s532542@nwmissouri.edu>
  *
