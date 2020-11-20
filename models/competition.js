1254af9519bb5f9767ccb5cd8581e239e2452cea
shivani <tangellapallyshivani@gmail.com>
2020-11-20 15:14:19 -0600

    commit

--- models/competition.js
+++ models/competition.js
@@ -13,40 +13,42 @@
 // It automatically receives the Sequelize connection parameter.
 
 module.exports = (db, DataTypes) => {
-  db.define('Competition', {
-    // sqlite creates a rowid attribute automatically
-    name: {
-      type: DataTypes.STRING(50),
-      defaultValue: 'NewCompetitionName'
-    },
-    creatorUserId: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "Users",
-        foreignKey: "id"
+  db.define(
+    'Competition',
+    {
+      // sqlite creates a rowid attribute automatically
+      name: {
+        type: DataTypes.STRING(50),
+        defaultValue: 'NewCompetitionName',
       },
-      defaultValue: 3,
+      creatorUserId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Users',
+          foreignKey: 'id',
+        },
+        defaultValue: 3,
+      },
+      questId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Quests',
+          foreignKey: 'id',
+        },
+        defaultValue: 1,
+      },
+      startDateTime: {
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+      },
+      endDateTime: {
+        type: DataTypes.DATE,
+        defaultValue: new Date(),
+      },
     },
-    questId: {
-      type: DataTypes.INTEGER,
-      references: {
-          model: "Quests",
-          foreignKey: "id"
-      }, 
-      defaultValue : 1,
-    },
-    startDateTime: {
-      type: DataTypes.DATE,
-      defaultValue: new Date(),
-    },
-     endDateTime: {
-      type: DataTypes.DATE,
-      defaultValue: new Date(),
-    },
-  },
-  {
-    // Other model options go here
-    freezeTableName: true, // table name will be model name - no plurals
-  }
+    {
+      // Other model options go here
+      freezeTableName: true, // table name will be model name - no plurals
+    }
   );
 };
